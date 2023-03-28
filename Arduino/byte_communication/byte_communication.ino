7daf5a404138b6687af3f191246949f57419397b
FlexzIF <nathan.dumoulin11@outlook.com>
2023-03-28 18:44:17 -0400

    Update byte_communication.ino

--- Arduino/byte_communication/byte_communication.ino
+++ Arduino/byte_communication/byte_communication.ino
@@ -1,9 +1,15 @@
+#include <Servo.h>
+
 void setup() {
   Serial.begin(9600); // Make sure the baud rate matches the Raspberry Pi side
+
+  servo1.attach(6);
+  servo1.write(servoAngle);
 }
 
 void loop() {
   if (Serial.available() > 0) {
+    servo1.writeMicroseconds(1600);
     int incomingByte = Serial.read();
     Serial.print("Received: ");
     Serial.println(incomingByte, DEC);
