aecd61a18c65525f1d9d16c6293a9c79555b6126
FlexzIF <nathan.dumoulin11@outlook.com>
2023-03-28 17:45:47 -0400

    xx

--- Arduino/byte_communication/byte_communication.ino
+++ Arduino/byte_communication/byte_communication.ino
@@ -1,11 +1,9 @@
-String nom = "Arduino";
-
 void setup() {
   Serial.begin(9600); // Make sure the baud rate matches the Raspberry Pi side
 }
 
 void loop() {
-  if (Serial.available()) {
+  if (Serial.available() > 0) {
     byte byte_received = Serial.read();
     Serial.write(byte_received);
   }
--- Scripts/byte_communication.py
+++ Scripts/byte_communication.py
@@ -2,7 +2,7 @@ import serial
 byte_to_send = 0x42 # Replace this with the byte you want to send
 
 with serial.Serial("/dev/ttyUSB0", 9600, timeout=1) as arduino:
-    arduino.write(b'\x01')
+    arduino.write(byte_to_send)
     
     if arduino.isOpen():
                 print("{} connected!". format(arduino.port))
