7b84307bad8a63f0bc8bf3d8b4fa92d2c61b7425
Ndumoulin <nathan.dumoulin11@outlook.com>
2023-05-12 12:46:17 -0400

    commit code complet sans la cam

--- Arduino/ControlArm/ControlArm.ino
+++ Arduino/ControlArm/ControlArm.ino
@@ -1,37 +1,134 @@
 #include <Servo.h>
 
-String nom = "Arduino";
+// Définition de tous les PIN nécessaire pour les moteurs DC
+#define enA 5
+#define enB 3
+#define in1 4
+#define in2 7
+#define in3 2
+#define in4 8
+
 String msg = "";
 
-int servoAngle = 90;
-int commands[6];
+bool drivingMode = true;
 
-Servo servo1;
+int waistAngle = 90;
+int shoulderAngle = 90;
+int elbowAngle = 90;
+
+int motorSpeedL= 0;
+int motorSpeedR = 0;
+
+int commands[7];
+
+int Solenoid = 9;
+
+Servo waistServo;
+Servo shoulderServo;
+Servo elbowServo;
 
 void setup() {
   Serial.begin(115200);
 
-  servo1.attach(6);
-  servo1.write(servoAngle);
+  pinMode(Solenoid, OUTPUT);
+
+  pinMode(enA, OUTPUT);
+  pinMode(enB, OUTPUT);
+  pinMode(in1, OUTPUT);
+  pinMode(in2, OUTPUT);
+  pinMode(in3, OUTPUT);
+  pinMode(in4, OUTPUT);
+  
+  waistServo.attach(11);
+  shoulderServo.attach(10);
+  elbowServo.attach(6);
 }
 
 void loop() {
   readSerialPort();
 
   if(msg != "") {
-    sendData();
     splitCommands(msg);
-    if(commands[3] == 1) {
-      servo1.write(180);
+
+    // Contrôle du moteur DC gauche
+    
+    if(commands[0] > 0) {
+      motorSpeedL = commands[0];
+      digitalWrite(in1, LOW);
+      digitalWrite(in2, HIGH);
+      analogWrite(enA, motorSpeedL);
     }
-    else if(commands[3] == 0) {
-      servo1.write(90);
+    else if (commands[0] < 0) {
+       motorSpeedL = commands[0] * -1;
+      digitalWrite(in1, HIGH);
+      digitalWrite(in2, LOW);
+      analogWrite(enA, motorSpeedL);
     }
-    else if(commands[3] == -1) {
-      servo1.write(0);
+    else if (commands[0] == 0) {
+      analogWrite(enA, 0);
+    }
+
+    // Contrôle du moteur DC droit
+    
+    if(commands[1] > 0) {
+      motorSpeedR = commands[1];
+      digitalWrite(in3, LOW);
+      digitalWrite(in4, HIGH);
+      analogWrite(enB, motorSpeedR);
+    }
+    else if (commands[1] < 0) {
+       motorSpeedL = commands[1] * -1;
+      digitalWrite(in3, HIGH);
+      digitalWrite(in4, LOW);
+      analogWrite(enB, motorSpeedR);
+    }
+    else if (commands[1] == 0) {
+      analogWrite(enB, 0);
     }
   }
-  delay(50);
+  
+  // Waist servo control
+  
+  if(commands[2] == 1 && waistAngle < 180) {
+    waistAngle = waistAngle + 1;
+    waistServo.write(waistAngle);
+  }
+  else if(commands[2] && waistAngle > 0) {
+    waistAngle = waistAngle - 1;
+    waistServo.write(waistAngle);
+  }
+
+  // Shoulder servo control
+
+  if(commands[3] == 1 && shoulderAngle < 180) {
+    shoulderAngle = shoulderAngle + 1;
+    shoulderServo.write(shoulderAngle);
+  }
+  else if(commands[3] == -1 && shoulderAngle > 0) {
+    shoulderAngle = shoulderAngle - 1;
+    shoulderServo.write(shoulderAngle);
+  }
+  
+  // Elbow servo control
+
+  if(commands[4] == 1 && elbowAngle < 180) {
+    elbowAngle = elbowAngle + 1;
+    elbowServo.write(elbowAngle);
+  }
+  else if(commands[4] == -1 && elbowAngle > 0) {
+    elbowAngle = elbowAngle - 1;
+    elbowServo.write(elbowAngle);
+  }
+
+  //Magnet control
+  if (commands[5] == 1) {
+    digitalWrite(Solenoid, HIGH);
+  } 
+  else if(commands[5] == 0) {
+    digitalWrite(Solenoid, LOW);
+  }
+
+  delay(20);
 }
 
 void splitCommands(String line) {
@@ -43,7 +140,7 @@ void splitCommands(String line) {
 
   token = strtok(buffer, ";");
 
-  while (token != NULL && index < 5) {
+  while (token != NULL && index < 6) {
     int value = atoi(token);
     commands[index] = value;
     index++;
--- Scripts/arm_control.py
+++ Scripts/arm_control.py
@@ -13,7 +13,8 @@ class XBOXController(object):
     button_data = None
     hat_data = None
     
-    commands = [0,0,0,0,0,0]
+    commands = [0,0,0,0,0,0,1]
+    drivingMode = True
     
 
     def init(self):
@@ -59,25 +60,75 @@ class XBOXController(object):
                     if event.type == pygame.JOYAXISMOTION:
                         self.axis_data[event.axis] = round(event.value,2)
                         
-                        if event.axis == 3 and self.axis_data[event.axis] == 0 and self.commands[3] != 0:
-                            self.commands[3] = 0
-                        if event.axis == 3 and self.axis_data[event.axis] >= 0.3 and self.commands[3] != -1:
-                            self.commands[3] = -1
-                        if event.axis == 3 and self.axis_data[event.axis] <= -0.3 and self.commands[3] != 1:
-                            self.commands[3] = 1
+                        if self.drivingMode == True:
+                            """LEFT DC motors control"""
+                            if event.axis == 1 and self.axis_data[event.axis] == 0 and self.commands[0] != 0:
+                                self.commands[0] = 0
+                            if event.axis == 1 and self.axis_data[event.axis] >= 0.5 and self.commands[0] != -100:
+                                self.commands[0] = -100
+                            if event.axis == 1 and self.axis_data[event.axis] <= -0.5 and self.commands[0] != 100:
+                                self.commands[0] = 100
+                                
+                            """RIGHT DC motors control"""
+                            if event.axis == 3 and self.axis_data[event.axis] == 0 and self.commands[1] != 0:
+                                self.commands[1] = 0
+                            if event.axis == 3 and self.axis_data[event.axis] >= 0.5 and self.commands[1] != -100:
+                                self.commands[1] = -100
+                            if event.axis == 3 and self.axis_data[event.axis] <= -0.5 and self.commands[1] != 100:
+                                self.commands[1] = 100
+                            
+                        if self.drivingMode == False:
+                            """Shoulder servo control"""
+                            
+                            if event.axis == 3 and self.axis_data[event.axis] == 0 and self.commands[3] != 0:
+                                self.commands[3] = 0
+                            if event.axis == 3 and self.axis_data[event.axis] >= 0.3 and self.commands[3] != -1:
+                                self.commands[3] = -1
+                            if event.axis == 3 and self.axis_data[event.axis] <= -0.3 and self.commands[3] != 1:
+                                self.commands[3] = 1
+                                
+                            """Elbow servo control"""
+                            
+                            if event.axis == 1 and self.axis_data[event.axis] == 0 and self.commands[4] != 0:
+                                self.commands[4] = 0
+                            if event.axis == 1 and self.axis_data[event.axis] >= 0.3 and self.commands[4] != -1:
+                                self.commands[4] = 1
+                            if event.axis == 1 and self.axis_data[event.axis] <= -0.3 and self.commands[4] != 1:
+                                self.commands[4] = -1
                         
                     elif event.type == pygame.JOYBUTTONDOWN:
                         self.button_data[event.button] = True
-                        if event.button == 7:
-                            self.commands[2] = 1
-                        if event.button == 6:
-                            self.commands[2] = -1
+                        
+                        """Changement de mode"""
+                        if event.button == 4 and self.commands[6] == 0:
+                            self.commands[6] = 1
+                            self.drivingMode = True
+                        elif event.button == 4 and self.commands[6] == 1:
+                            self.commands[6] = 0
+                            self.drivingMode = False
+                        
+                        if self.drivingMode == False:
+                            """Waist servo control"""
+                            
+                            if event.button == 7:
+                                self.commands[2] = -1
+                            if event.button == 6:
+                                self.commands[2] = 1
+                                
+                        """Magnet control"""
+                        if event.button == 3:
+                            if self.commands[5] == 0:
+                                self.commands[5] = 1
+                            elif self.commands[5] == 1:
+                                self.commands[5] = 0
+                                
                     elif event.type == pygame.JOYBUTTONUP:
                         self.button_data[event.button] = False
                         if event.button == 7:
                             self.commands[2] = 0
                         if event.button == 6:
                             self.commands[2] = 0
+                            
                     elif event.type == pygame.JOYHATMOTION:
                         self.hat_data[event.hat] = event.value
                         
@@ -85,68 +136,15 @@ class XBOXController(object):
                         arduino.write(str.encode(self.generate_command()))
                         print(str.encode(self.generate_command()))
                         
-                
+      
+def convert_data(x):
+    return int(x * 255)
+    
 
 def controller_main():
     xbox = XBOXController()
     xbox.init()
     xbox.listen()
-    
+
 
 controller_main()
-
-"""
-pygame.init()
-pygame.joystick.init()
-
-try:
-    controller = pygame.joystick.Joystick(0)
-    controller.init()
-    
-except:
-    print("WRONG")
-
-#joysticks = []
-
-#for i in range(0, pygame.joystick.get_count()):
-#   joysticks.append(pygame.joystick.Joystick(i))
-#    joysticks[-1].init()
-
-if __name__ == '__main__':
-
-    with serial.Serial("/dev/ttyUSB0", 9600, timeout=1) as arduino:
-        time.sleep(0.1)
-
-        if arduino.isOpen():
-            print("{} connected!". format(arduino.port))
-
-            try:
-                while True:
-
-                    #check for joystick events
-
-                    for event in pygame.event.get():
-                        if event.type == JOYBUTTONDOWN:
-                            if event.button == 7:
-                                arduino.write(b"RB")
-                                print("Button RB down")
-                            if event.button == 6:
-                                arduino.write(b"LB")
-                                print("Button LB down")
-                                
-                        if event.type == JOYAXISMOTION:
-                            if controller.get_axis(2) > 0.5 :
-                                #print(controller.get_axis(2))
-                                arduino.write(b"A,1")
-                                print("axis")
-                            if controller.get_axis(2) < 0.5 :
-                                #print(controller.get_axis(2))
-                                arduino.write(b"A,-1")
-                                print("axis")
-                                
-                    
-                            
-                            
-            except KeyboardInterrupt:
-                print("KeyboardInterrupt has been caught.")
-"""
